574be5ab2b7098551dfffd2118201b82be22c88b
vladyslavhaidaienko <gvgnexus5@gmail.com>
2021-08-29 10:51:15 +0300

    abc

---
+++ HelloWorld1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld1
+//
+//  Created by Vladyslav Haidaienko on 28.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
